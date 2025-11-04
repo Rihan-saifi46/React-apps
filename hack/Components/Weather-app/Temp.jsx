2604ab27ef3533ef49a709197a7e93168fbce38d
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-04 23:01:26 +0530

    add

---
+++ hack/Components/Weather-app/Temp.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Temp = () => {
+  return (
+    <>
+    <div className="box text-4xl">
+        <div className="input">
+            <input type="text" />
+        </div>
+        <div className="location">
+            <h1>London</h1>
+        </div>
+        <div className="info">
+            <p>48 deg</p>
+            <p>27%</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Temp
