c6c74f14861428b665f55863f33ea386264559b1
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-05 20:34:42 +0530

    add

--- hack/Components/Quote-gen/Quote.jsx
+++ hack/Components/Quote-gen/Quote.jsx
@@ -3,14 +3,29 @@ import React,{useState} from 'react'
 
 const Quote = () => {
     const [quote, setQuote] = useState()
+
+    const Generator = async () => {
+     try {
+        const fetch = await fetch('https://api.quotable.io/random')
+        const data = fetch.json()
+        
+        setQuote(data)
+        console.log(quote);      
+
+     } catch (error) {
+        console.log("quote not found");
+        setQuote({content:"failed to load quote",authoe:"unknown"})
+     }
+    }
   return (
    <>
    <div>
-   <p className='text-2xl'>my name is rihan</p>
+   <p className='text-2xl'>rihan</p>
    </div>
    <div className='flex gap-[2vw] align-middle'>
     <p className='font-bold text-3xl'>author name</p>
-   <button>New Quote</button>
+   <button className='p-[1vw] rounded border-2' 
+   onClick={Generator}>New Quote</button>
    </div>
    
    </>
