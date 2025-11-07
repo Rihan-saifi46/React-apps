988366dd9d11f36681507a4e67b3568806044d5e
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-07 10:00:00 +0530

    7 Nov commit

--- hack/Components/Navbar+dark node/Pnav.jsx
+++ hack/Components/Navbar+dark node/Pnav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Menu, X, Sun, Moon } from 'lucide-react';
 
 const Pnav = () => {
     const [isDark1, setIsDark1] = useState(false)
@@ -16,7 +17,7 @@ const Pnav = () => {
    <>
    <div className={isDark1 ? "bg-gray-950 min-h-screen":"bg-amber-50 min-h-screen"}>
      <nav className='flex justify-between px-[2vw] py-[.5vw] shadow-lg shadow-black-500/50'>
-      <div className={isDark1 ? "text-white text-3xl":"text-black text-3xl flex items-center"}>
+      <div className={isDark1 ? "text-white text-3xl":"text-black text-[2vw] flex items-center"}>
         My Brand
       </div>
       <div className={isDark1 ? "text-white text-[2vw] flex gap-[2vw]":"text-black text-[1.7vw] flex gap-[3vw] "}>
@@ -24,7 +25,13 @@ const Pnav = () => {
         <a href="#" className='no-underline'>About</a>
         <a href="#" className='no-underline'>Contact</a>
         <a href="#" className='no-underline'>Service</a>
+
+        <button onClick={dark} className={`p-2 rounded-lg ${isDark1 ? "bg-gray-700 text-yellow-400":"bg-amber-50 text-black"}  `}>
+            {isDark1 ? <Sun size={20}/>:<Moon size={20}/>}
+        </button>
       </div>
+      {/* button  */}
+      
      </nav>
    </div>
    </>
