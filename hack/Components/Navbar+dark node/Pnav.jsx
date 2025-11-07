5e1f4fd2176b1f179a6396d57fc1478b3d333902
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-07 10:00:00 +0530

    7 Nov commit

--- hack/Components/Navbar+dark node/Pnav.jsx
+++ hack/Components/Navbar+dark node/Pnav.jsx
@@ -15,7 +15,14 @@ const Pnav = () => {
   return (
    <>
    <div className={isDark1 ? "bg-gray-950 min-h-screen":"bg-amber-50 min-h-screen"}>
-    
+     <nav className='flex justify-between px-[1vw]'>
+      <div className={isDark1 ? "text-white text-3xl":"text-black text-3xl"}>
+        My Brand
+      </div>
+      <div className={isDark1 ? "text-white text-[2vw]":"text-black text-[2vw]"}>
+        <a href="#" className=''>Home</a>
+      </div>
+     </nav>
    </div>
    </>
   )
