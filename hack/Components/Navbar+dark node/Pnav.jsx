cb3adf8514dded881fe827f2d534094610070f04
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-07 10:00:00 +0530

    7 Nov commit

--- hack/Components/Navbar+dark node/Pnav.jsx
+++ hack/Components/Navbar+dark node/Pnav.jsx
@@ -15,12 +15,15 @@ const Pnav = () => {
   return (
    <>
    <div className={isDark1 ? "bg-gray-950 min-h-screen":"bg-amber-50 min-h-screen"}>
-     <nav className='flex justify-between px-[1vw]'>
-      <div className={isDark1 ? "text-white text-3xl":"text-black text-3xl"}>
+     <nav className='flex justify-between px-[2vw] py-[.5vw] shadow-lg shadow-black-500/50'>
+      <div className={isDark1 ? "text-white text-3xl":"text-black text-3xl flex items-center"}>
         My Brand
       </div>
-      <div className={isDark1 ? "text-white text-[2vw]":"text-black text-[2vw]"}>
-        <a href="#" className=''>Home</a>
+      <div className={isDark1 ? "text-white text-[2vw] flex gap-[2vw]":"text-black text-[1.7vw] flex gap-[3vw] "}>
+        <a href="#" className='no-underline'>Home</a>
+        <a href="#" className='no-underline'>About</a>
+        <a href="#" className='no-underline'>Contact</a>
+        <a href="#" className='no-underline'>Service</a>
       </div>
      </nav>
    </div>
--- hack/src/App.jsx
+++ hack/src/App.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 // import Todo from '../Components/Todo-app/Todo'
 // import Temp from '../Components/Weather-app/Temp'
 // import Quote from '../Components/Quote-gen/Quote'
-import Nav from '../Components/Navbar+dark node/Nav'
+// import Nav from '../Components/Navbar+dark node/Nav'
+import Pnav from '../Components/Navbar+dark node/Pnav'
 
 const App = () => {
   return (
     <div>
-      <Nav/>
+      <Pnav/>
     </div>
   )
 }
