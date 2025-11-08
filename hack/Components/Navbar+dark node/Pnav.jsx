64a4381002bd76c2d895fdb2c74f48620dfba52f
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-08 10:00:00 +0530

    8 Nov commit

--- hack/Components/Navbar+dark node/Pnav.jsx
+++ hack/Components/Navbar+dark node/Pnav.jsx
@@ -16,7 +16,7 @@ const Pnav = () => {
   return (
    <>
    <div className={isDark1 ? "bg-gray-950 min-h-screen":"bg-amber-50 min-h-screen"}>
-     <nav className='flex justify-between px-[2vw] py-[.5vw] shadow-lg shadow-black-500/50'>
+     <nav className='flex justify-between px-[2vw] py-[.5vw] '>
       <div className='flex justify-between w-full'>
         <div className={isDark1 ? "text-white text-3xl sm:text-2xl":"text-black text-[2vw] flex items-center sm:text-2xl"}>
         My Brand
@@ -48,25 +48,27 @@ const Pnav = () => {
 
      {
       isOpen1 && (
-        <div >
-         <div className={`md:hidden  ${isDark1 ? "text-white text-[2vw] flex gap-[2vw]":"text-black text-[1.7vw] flex gap-[3vw] "}`}>
+        <div className=''>
+         <div className={`md:hidden bg-amber-50 relative ${isDark1 ? "text-white text-[1.7vw] flex gap-[2vw]":"text-black text-[1.7vw] flex gap-[2vw]"}`}>
        {/* button  */}
          {/* <button onClick={dark} className={`p-2 rounded-lg ${isDark1 ? "bg-gray-700 text-yellow-400":"bg-amber-50 text-black"}  `}>
             {isDark1 ? <Sun size={20}/>:<Moon size={20}/>}
         </button> */}
         {/* button  */}
-
+       <div className=' pt-1.5 w-screen flex flex-col text-2xl gap-[2vw] absolute top-[5vw] right-[-2vw] px-4 transition '>
         <a href="#" className='no-underline'>Home</a>
         <a href="#" className='no-underline'>About</a>
         <a href="#" className='no-underline'>Contact</a>
         <a href="#" className='no-underline'>Service</a>
+       </div>
+      
 
       </div>
         </div>
       )
      }
      </nav>
-
+   
      
    </div>
    </>
